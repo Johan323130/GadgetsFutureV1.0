7fc3ff29fc49b79a9da071a3a45028690b7d1797
Johan323130 <njohan205@gmail.com>
2024-04-15 12:00:44 -0500

    Actualización 15/04/24

--- app/src/main/java/com/example/gadgetsfuture/config/config.kt
+++ app/src/main/java/com/example/gadgetsfuture/config/config.kt
@@ -12,10 +12,10 @@ class config {
         var token:String = ""
         //crear una variable para almacenar la url base
         //puerto SENA:
-        //const val urlBase="http://10.192.88.39:8000"
+        const val urlBase="http://192.168.137.1:8000"
         //const val urlBase="http://192.168.42.32:8000"
         //puerto local:
-        const val urlBase="http://192.168.1.12:8000"
+        //const val urlBase="http://192.168.1.12:8000"
         //const val urlBase="http://192.168.105.200:8000"
 
         const val urlTienda="${urlBase}/tienda/api/"
